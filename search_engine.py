df09528b1cb1f926f3a81a37f3eddcd487499d48
Vladimir_Kuznetsov <Vladimir_Kuznetsov2@epam.com>
2019-12-23 16:55:19 +0400

    Отладка heroku: - исправлена опечатка в названии ключа для картинок

--- search_engine.py
+++ search_engine.py
@@ -9,10 +9,8 @@ INFO_PREFIX = "фильм"
 def search_watch(message, cfg):
     params = {"q": f"{message} {WATCH_QUERY_FIXTURE}"}
     params.update(cfg["search_engine"])
-    print(params)
     client = GoogleSearchResults(params)
     result = client.get_dict()
-    print(result)
     return result["organic_results"][0]["link"]
 
 def search_info(message, cfg):
@@ -25,8 +23,6 @@ def search_poster(message, cfg):
     params = {"q": message}
     params.update(cfg["search_engine"])
     params.update({"tbm": "isch"}) # image search
-    print(params)
     client = GoogleSearchResults(params)
     result = client.get_dict()
-    print(result)
-    return result["image_results"][0]["original"]
+    return result["images_results"][0]["original"]
